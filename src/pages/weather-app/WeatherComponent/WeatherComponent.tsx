7147d54282ba78a714e6bdaca5dd141ecfe5cfc8
Gopi saravanan R <gopisaravanan1398@gmail.com>
2026-02-01 22:25:28 +0530

    weather app

--- src/pages/weather-app/WeatherComponent/WeatherComponent.tsx
+++ src/pages/weather-app/WeatherComponent/WeatherComponent.tsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from "react";
+
 const WeatherComponent = () => {
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://mocki.io/v1/2f766377-7427-4c52-bf49-904fcc37993c",
+    );
+    const data = await response.json();
+    console.log(response);
+    setData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
   return (
     <>
       <h3>Weather App</h3>
+      <div>
+        {Object.entries(data).map(([city, info]) => (
+          <div>
+            <h3>{city}</h3>
+            <p>Temp: {info.temperature}°C</p>
+            <p>Condition: {info.condition}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 };
 
 export default WeatherComponent;
